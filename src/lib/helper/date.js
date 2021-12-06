fe094f7fa2439664705e228a0033010c976bf3f8
Amir Salehi <amirsalehi@Amirs-MacBook-Pro.local>
2021-12-06 16:24:34 +0330

    helper removed folder

--- src/lib/helper/date.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-const JalaliMoment = require('moment-jalaali');
-
-export function getDaysAgoFromTimestamp(timestamp) {
-    moment.locale('fa');
-    return JalaliMoment(timestamp).fromNow().replace("days ago", "");
-}
